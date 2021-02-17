21feb39b63e0265d5659fe3ef9397484517c6906
David <58638088+deved9@users.noreply.github.com>
2021-02-17 11:14:38 +0100

    added comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
-
 import discord
-import random
 from pyngrok import ngrok,exception
 import os
 from time import sleep
 
+#paste ngrok authtoken here
 ngrok.set_auth_token("")
 
+#prefix for bots commands
 prefix = "--"
 
-prikazy = ["help" , "ngrok"]
+#list of available commands to print when asked for help
+commands = ["help" , "ngrok"]
 
 client = discord.Client()
 
@@ -18,27 +18,30 @@ client = discord.Client()
 async def on_ready():
     print("bot is online!")
     try:
+        #here you can add your custom ngrok tunnels
         ngrok.connect(22,proto="tcp",region="eu")
-    except Exception:
-        print("Exception occured, reloading")
+    except Exception as e:
+        print(f"Exception occured, reloading\n{e}")
         sleep(15)
         on_ready()
 
 @client.event
 async def on_message(message):
     channel = message.channel
+    
+    #only works in channel with name "ssh"
     if str(channel) == "ssh":
-        if message.content == f"{prefix}{prikazy[0]}":              # --help
-            string = "Dostupné příkazy:\n"
-            for prikaz in prikazy:
-                string += f"  {prefix}{prikaz}\n"
+        if message.content == f"{prefix}{commands[0]}":              # --help
+            string = "Available commands:\n"
+            for command in commands:
+                string += f"  {prefix}{command}\n"
             await channel.send(string)
-        elif message.content == f"{prefix}{prikazy[1]}":            # --ngrok
+        elif message.content == f"{prefix}{commands[1]}":            # --ngrok
             tunels = ngrok.get_tunnels()
-            string = "Aktivní tunely:\n"
+            string = "Active tunnels:\n"
             for tunel in tunels:
                 string += f"  {tunel}\n"
             await channel.send(string)
 
-
+#paste discord bot token here
 client.run("")
